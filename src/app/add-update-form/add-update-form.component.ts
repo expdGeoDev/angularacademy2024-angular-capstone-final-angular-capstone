82f8dd178405fe815cc7cb3e4d141e75105de6d9
Dylan McClain <chq-dylanm@corp.expeditors.com>
2024-07-15 10:58:00 -0700

    add routing config and route for add-update form

---
+++ src/app/add-update-form/add-update-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: 'add-update-form',
+	standalone: true,
+	imports: [],
+	templateUrl: './add-update-form.component.html',
+	styleUrl: './add-update-form.component.css'
+})
+export class AddUpdateFormComponent { }
