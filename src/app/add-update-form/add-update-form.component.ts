4880f6a0a3c0e2568bf245b525db1734941e76e7
Dylan McClain <chq-dylanm@corp.expeditors.com>
2024-07-16 10:56:24 -0700

    form WIP

--- src/app/add-update-form/add-update-form.component.ts
+++ src/app/add-update-form/add-update-form.component.ts
@@ -1,10 +1,28 @@
 import {Component} from "@angular/core";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
 	selector: 'add-update-form',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: './add-update-form.component.html',
 	styleUrl: './add-update-form.component.css'
 })
-export class AddUpdateFormComponent { }
+export class AddUpdateFormComponent {
+	coffeeForm = new FormGroup({
+		roasterControl: new FormControl('', Validators.required),
+		varietyControl: new FormControl(null),
+		sizeControl: new FormControl(null, Validators.required),
+		roastControl: new FormControl(null, Validators.required),
+		formatControl: new FormControl(null, Validators.required),
+		grindControl: new FormControl(null),
+		originControl: new FormControl(null),
+		singleOriginControl: new FormControl(false),
+		tastingNotesControl: new FormControl('')
+	})
+
+	submitForm() {
+		let result = this.coffeeForm.getRawValue().roasterControl;
+		alert(result);
+	}
+}
