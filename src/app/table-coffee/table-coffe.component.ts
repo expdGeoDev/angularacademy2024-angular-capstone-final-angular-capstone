90d6252ab6afd64ddf9c59927cf7d5422cb0e3e0
Javir Mendoza <Javier.Mendoza@expeditors.com>
2024-07-16 12:03:07 -0500

    update Coffee table

--- src/app/table-coffee/table-coffe.component.ts
+++ src/app/table-coffee/table-coffe.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './table-coffe.component.html',
-  styleUrl: './table-coffe.component.css'
+  styleUrl: './table-coffee.component.css'
 })
 export class TableCoffeComponent {
 
