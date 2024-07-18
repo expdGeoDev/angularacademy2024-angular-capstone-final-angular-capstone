364830fa23cb7df9060102bbab620e64428d98b0
chris valencia <christopher.valencia@expeditors.com>
2024-07-18 10:31:21 -0700

    added  table to deleted coffees

--- src/app/coffee-deleted/coffee-deleted.component.ts
+++ src/app/coffee-deleted/coffee-deleted.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Coffee } from '../common/coffee-model';
+import { CoffeeHttpService } from '../services/coffee-http.service';
 
 @Component({
   selector: 'app-coffee-deleted',
   standalone: true,
-  imports: [],
+	imports: [
+		NgForOf,
+		NgxPaginationModule,
+	],
   templateUrl: './coffee-deleted.component.html',
   styleUrl: './coffee-deleted.component.css'
 })
-export class CoffeeDeletedComponent {
+export class CoffeeDeletedComponent implements OnInit{
+	p=0;
+	coffeeData! : Coffee[];
+	constructor(private coffeeHttp: CoffeeHttpService) {
+	}
+
+	ngOnInit() {
+
+		this.coffeeHttp.getAllCoffees().subscribe(
+			{next:(data)=>{
+					this.coffeeData = data;
+					console.log(this.coffeeData);
+				}
+			})
+	}
 
 }
