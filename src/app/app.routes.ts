8c67ff2740ec987ceb42f785aa2262d7d2ed3fe3
chris valencia <christopher.valencia@expeditors.com>
2024-07-18 09:25:00 -0700

    routing confirmed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Ng2StateDeclaration } from "@uirouter/angular";
 import { CoffeeDeletedComponent } from './coffee-deleted/coffee-deleted.component';
+import { AppComponent } from './app.component';
 
 export const routes: Ng2StateDeclaration[] = [
+	{
+		name: 'home',
+		url: '/home',
+		component: AppComponent,
+	},
 	{
 		name: 'coffeeDelete',
 		url: '/coffee-delete',
